9de87def9a021571ba8fbb3cfc6291aa65ef0b87
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-07-08 17:57:59 -0300

    se agrega doble div en el panel derecho

--- scripts/script.js
+++ scripts/script.js
@@ -9,41 +9,60 @@ function showAlert(message) {
 }
 
 function encriptar() {
-    let inputText = document.getElementById("inputText").value;
-    console.log(inputText);
+  let inputText = document.getElementById("inputText").value;
+  let outputText = "";
+  console.log(inputText);
+
+  if (inputText.trim() === "") {
+    showNoTextMessage();
+  } else {
     if (!/^[a-z\s]+$/.test(inputText)) {
-        showAlert("Por favor, ingresa solo letras minúsculas y sin acentos.");
-        return;
+      showAlert("Por favor, ingresa solo letras minúsculas y sin acentos.");
+      return;
     }
     // Reemplazos según las reglas proporcionadas
-    let outputText = inputText
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
+    outputText = inputText.replace(/e/g, "enter")
+                          .replace(/i/g, "imes")
+                          .replace(/a/g, "ai")
+                          .replace(/o/g, "ober")
+                          .replace(/u/g, "ufat");
 
-    document.getElementById("outputText").value = outputText;
+    document.getElementById("resultingText").value = outputText;
+    showOutputText();
+  }
+}
+function desencriptar() {
+  let inputText = document.getElementById("inputText").value;
+  let outputText = "";
+
+  if (inputText.trim() === "") {
+    showNoTextMessage();
+  } else {
+    // Reemplazar las secuencias encriptadas por las letras originales
+    outputText = inputText.replace(/enter/g, "e")
+                          .replace(/imes/g, "i")
+                          .replace(/ai/g, "a")
+                          .replace(/ober/g, "o")
+                          .replace(/ufat/g, "u");
+
+    document.getElementById("resultingText").value = outputText;
+    showOutputText();
+  }
+}
+function showNoTextMessage() {
+  document.getElementById("noTextMessage").style.display = "block";
+  document.getElementById("outputText").style.display = "none";
 }
 
-function desencriptar() {
-    let inputText = document.getElementById("inputText").value;
-
-    // Reemplazos inversos según las reglas proporcionadas
-    let outputText = inputText
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
-
-    document.getElementById("outputText").value = outputText;
+function showOutputText() {
+  document.getElementById("noTextMessage").style.display = "none";
+  document.getElementById("outputText").style.display = "block";
 }
 
 function copiarTexto() {
   // Obtiene el textarea del output
   let outputText = document.getElementById("outputText").value;
-  
+
   // Usa la API del Portapapeles para copiar el texto
   navigator.clipboard.writeText(outputText).then(function () {
     // Muestra una alerta personalizada para indicar que el texto fue copiado
